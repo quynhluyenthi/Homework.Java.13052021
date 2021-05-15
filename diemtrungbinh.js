99196791e4cf125b156ca0f685e8d3148eba47f6
quynhluyenthi <quynhluyenthi@gmail.com>
2021-05-15 16:22:06 +0700

    Diem trung binh

---
+++ diemtrungbinh.js
@@ -0,0 +1,6 @@
+  
+let Toan = +prompt('Nhập vào điểm Toán:');
+let Van = +prompt('Nhập vào điểm Văn:');
+let Anh = +prompt('Nhập vào điểm Tiếng Anh:');
+Ave = (Toan + Van + Anh) / 3;
+alert("Điểm trung bình 3 môn là: " +Ave);
